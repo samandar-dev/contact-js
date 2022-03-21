75f54eaae88ab79c311f7e626f787afc3a5b8dc8
samandar-dev <samandarbohodirov01@gmail.com>
2022-03-21 21:07:09 +0500

    addd

--- js/main.js
+++ js/main.js
@@ -3,21 +3,75 @@ const workBtn = document.querySelector("#workBtn");
 const familyBtn = document.querySelector("#familyBtn");
 
 const conForm = document.querySelector("#conForm")
-const conlist = document.querySelector("#conlist")
-const addBtn = document.querySelector("#addBtn");
+const conlistAll = document.querySelector("#conlistAll")
+const conlistWork = document.querySelector("#conlistWork")
+const conlistFam = document.querySelector("#conlistFam")
 const select = document.querySelector("#select");
 
 const last = document.querySelector("#last");
 const first = document.querySelector("#first");
 const number = document.querySelector("#number");
 
-const allVal = document.querySelector("#allVal");
-const workVal = document.querySelector("#workVal");
-const familyVal = document.querySelector("#familyVal");
 
+let arr = [];
+let allArr = [];
+let workArr = [];
+let famArr = [];
+
+let h_arr = [
+  {
+    lastname: 'John',
+    firstname: 'Doe',
+    categor: 'Family',
+    number: '8 765 43 21',
+  },
+  {
+    lastname: 'Tom',
+    firstname: 'Hardy',
+    categor: 'Work',
+    number: '8 765 43 21',
+  },
+  {
+    lastname: 'Jamoliddin',
+    firstname: 'Najmiddinov',
+    categor: 'All',
+    number: '8 765 43 21',
+  }
+]
+
+let foreachh = h_arr.forEach((e) => {
+  let li_i = document.createElement('li');
+  li_i.className = "con__item";
+  li_i.innerHTML = `
+  <div class="con__item-box d-flex align-items-center">
+  <p class="conn-namee">${e.categor} contact</p>
+  <div class="con__icon">
+  <i class='bx bxs-contact'></i>
+  </div>
+  
+  <div class="con__names">
+  <p class="ite__tit">${e.lastname}</p>
+  <p class="ite__tit">${e.firstname}</p>
+  <a class="ite__link" href="tel:123456789">+998 9${e.number}</a>
+  </div>
+  </div>`;
+
+  if (e.categor == 'All') {
+    conlistAll.appendChild(li_i);
+  }
+  if (e.categor == 'Work') {
+    conlistWork.appendChild(li_i);
+  }
+  if (e.categor == 'Family') {
+    conlistFam.appendChild(li_i);
+  }
+});
+
+let removeBtn = document.querySelector('.remove');
+
+categores();
 
 conForm.addEventListener('submit', addContacts);
-
 
 function addContacts(e) {
   e.preventDefault();
@@ -25,92 +79,73 @@ function addContacts(e) {
   let lastValue = last.value;
   let firstValue = first.value;
   let numberValue = number.value;
+  let vall = select.value
 
-
-
-
-  let object = {
-    lastname: `${lastValue}`,
-    firstname: `${firstValue}`,
-    number: `${numberValue}`
+  let obj = {
+    lastname: lastValue,
+    firstname: firstValue,
+    categor: vall,
+    number: numberValue,
   }
 
-
-  console.log(object);
+  arr.push(obj);
 
   let li = document.createElement('li');
-
   li.className = "con__item";
-  let conItemBox = document.createElement('div')
-  conItemBox.className = "con__item-box d-flex align-items-center";
-  conItemBox.innerHTML = `<div class="con__icon">
-      <i class='bx bxs-contact'></i>
-      </div>
-      <div class="con__names">
-        <p class="ite__tit">${lastValue}</p>
-        <p class="ite__tit">${firstValue}</p>
-        <a class="ite__link" href="tel:123456789">+998 9${numberValue}</a>
+
+  let foreach = arr.forEach((e) => {
+    li.innerHTML = `
+      <div class="con__item-box d-flex align-items-center">
+        <p class="conn-namee">${e.categor} contact</p>
+        <div class="con__icon">
+          <i class='bx bxs-contact'></i>
+        </div>
+
+        <div class="con__names">
+          <p class="ite__tit">${e.lastname}</p>
+          <p class="ite__tit">${e.firstname}</p>
+          <a class="ite__link" href="tel:123456789">+998 9${e.number}</a>
+        </div>
       </div>`;
+  });
 
+  conlistAll.appendChild(li);
 
-  li.appendChild(conItemBox)
-  conlist.appendChild(li)
-
-
-  let nat = false
-
-  allVal.addEventListener('click', () => {
-    li.remove();
-  })
-
-  workBtn.addEventListener('click', () => {
-    conItemBox.classList.add('Work')
-  })
-  familyBtn.addEventListener('click', () => {
-    conItemBox.classList.add('Family')
-  })
-
-  workBtn.addEventListener('click', workFilter);
-
-  // allBtn.addEventListener('click', () => {
-  //   if (select.value == "All") {
-  //     li.remove();
-  //   }
-  // })
-
-
-  function workFilter(e) {
-    let options = select.childNodes;
-
-    for (let i = 0; i < options.length; i++) {
-      let option = options[i]
-
-      switch (e.target.value) {
-        case "All":
-          option.style.display = 'flex'
-          break;
-        case "Work":
-          if (option.classList.contains("Work")) {
-            option.style.display = 'flex'
-          } else {
-            option.style.display = 'none'
-          }
-          break;
-        case "Family":
-          if (!option.classList.contains("Work")) {
-            option.style.display = 'flex'
-          } else {
-            option.style.display = 'none'
-          }
-          break;
-      }
+  workArr = arr.filter((e) => {
+    if (e.categor == 'Work') {
+      conlistWork.appendChild(li);
     }
-  }
+  })
 
+  famArr = arr.filter((e) => {
+    if (e.categor == 'Family') {
+      conlistFam.appendChild(li);
+    }
+  })
+
+  categores();
 
   last.value = "";
   first.value = "";
   number.value = "";
 }
 
+function categores() {
+  allBtn.addEventListener('click', () => {
+    conlistAll.classList.remove('d-none');
+    conlistWork.classList.remove('d-none');
+    conlistFam.classList.remove('d-none');
+  })
 
+  workBtn.addEventListener('click', () => {
+    conlistWork.classList.remove('d-none');
+    conlistFam.classList.add('d-none');
+    conlistAll.classList.add('d-none');
+  })
+
+  familyBtn.addEventListener('click', () => {
+    conlistFam.classList.remove('d-none');
+    conlistWork.classList.add('d-none');
+    conlistAll.classList.add('d-none');
+  })
+}
